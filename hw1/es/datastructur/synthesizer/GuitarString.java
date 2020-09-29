6b1bf5e418c9bbafe54a2637ae63d7507eb40664
Derek Tang <tangderek702@berkeley.edu>
2020-09-29 00:01:15 -0700

    not quite done

--- hw1/es/datastructur/synthesizer/GuitarString.java
+++ hw1/es/datastructur/synthesizer/GuitarString.java
@@ -13,10 +13,6 @@ public class GuitarString {
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this division operation into an int. For
-        //       better accuracy, use the Math.round() function before casting.
-        //       Your should initially fill your buffer array with zeros.
         buffer = new ArrayRingBuffer<Double>((int) Math.round(SR/frequency));
         for (int i = 0; i < buffer.capacity(); i++) {
             buffer.enqueue(0.00);
@@ -26,10 +22,6 @@ public class GuitarString {
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in buffer, and replace with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
-        //
         //       Make sure that your random numbers are different from each
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
@@ -47,9 +39,6 @@ public class GuitarString {
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       Do not call StdAudio.play().
         double holdFront = buffer.dequeue();
         double avgByDecay = ((holdFront+ buffer.peek())/2) * DECAY;
         buffer.enqueue(avgByDecay);
@@ -57,8 +46,6 @@ public class GuitarString {
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
         return buffer.peek();
     }
 }
-    // TODO: Remove all comments that say TODO when you're done.
